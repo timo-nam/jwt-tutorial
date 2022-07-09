be6b1eeb6c7b54ccbbcd820da0cad398e09aeaf6
Timo Nam <dwbh7358@gmail.com>
2022-07-09 16:14:27 +0900

    add: WebSecurityCustomizer into SecurityConfig

--- src/main/java/com/makusmile/jwttutorial/config/SecurityConfig.java
+++ src/main/java/com/makusmile/jwttutorial/config/SecurityConfig.java
@@ -3,10 +3,18 @@ package com.makusmile.jwttutorial.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer  webSecurityCustomizer() {
+        return (web) -> web
+                .ignoring()
+                .antMatchers("/h2-console/**", "/favicon.ico");
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
