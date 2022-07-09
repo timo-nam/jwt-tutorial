9e0f1b5ef8005a24e0d4611ea704d7433c16459a
Timo Nam <dwbh7358@gmail.com>
2022-07-09 22:00:01 +0900

    add: UserRepository

--- src/main/java/com/makusmile/jwttutorial/repository/UserRepository.java
+++ src/main/java/com/makusmile/jwttutorial/repository/UserRepository.java
@@ -1,2 +1,13 @@
-package com.makusmile.jwttutorial.repository;public interface UserRepository {
+package com.makusmile.jwttutorial.repository;
+
+import com.makusmile.jwttutorial.domain.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<User> findOneWithAuthoritiesByUsername(String username);
 }
