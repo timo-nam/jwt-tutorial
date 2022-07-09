25d529342515bccce71f45caa51e4d7e7a541581
Timo Nam <dwbh7358@gmail.com>
2022-07-09 11:48:50 +0900

    initial commit

---
+++ src/test/java/com/makusmile/jwttutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makusmile.jwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
